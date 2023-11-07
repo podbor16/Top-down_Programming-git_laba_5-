26c63cf442188302e6d16e1fe63b7797de436226
podbor16 <podbor250718@gmail.com>
2023-11-07 16:47:13 +0700

    Second_Stage

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from lorem_text import lorem
 
 def analysis_text(text):
+    # словарь для хранения частот символов
+    frequency = {}
+    # Подсчитываем общее число символов в тексте
+    count_symbols = len(text)
+    # Проходимся по каждому символу в тексте
+    for symbol in text:
+        if symbol in frequency:
+            frequency[symbol] += 1
+        else:
+            frequency[symbol] = 1
+    # Преобразуем частоты символов в пары (символ, частота встречаемости)
+    result = [(symbol, frequency / count_symbols) for symbol, frequency in frequency.items()]
+    return result
+
+def hand_text():
+    return
+
+def auto_text():
     return
 
 def menu():
@@ -18,11 +36,15 @@ def menu():
         elif choose == "2":
             pass
         elif choose == "3":
-            pass
+            result = analysis_text(text)
+            for symbol, frequency in result:
+                print(f"{symbol} - {frequency:.4f}")
+            input("Нажмите Enter, чтобы продолжить...")
         elif choose == "4":
-           pass
+            print("Выход из программы.")
+            break
         else:
-           pass
+           print("Некорректный выбор. Пожалуйста, выберите снова.")
 
 if __name__ == "__main__":
     menu()
