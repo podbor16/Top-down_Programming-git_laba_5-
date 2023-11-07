c7a68e0b701711ae65e7fa49da32aed6398a12ef
podbor16 <podbor250718@gmail.com>
2023-11-07 16:33:44 +0700

    First_Stage

---
+++ main.py
@@ -0,0 +1,28 @@
+from lorem_text import lorem
+
+def analysis_text(text):
+    return
+
+def menu():
+    text = ""
+    while True:
+        print("Главное меню:")
+        print("1. Ввести текст вручную.")
+        print("2. Ввести текст случайным образом.")
+        print("3. Решение задачи.")
+        print("4. Выход из программы ")
+        choose = input("Выберите пункт меню: ")
+
+        if choose == "1":
+            pass
+        elif choose == "2":
+            pass
+        elif choose == "3":
+            pass
+        elif choose == "4":
+           pass
+        else:
+           pass
+
+if __name__ == "__main__":
+    menu()
